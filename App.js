f2e5d53b7f827ecab15f288361aebfe7f663340e
Frazior, Carla <carla.frazior@elekta.com>
2015-03-04 18:28:53 -0800

    Add Iteration Filter

--- App.js
+++ App.js
@@ -2,38 +2,83 @@ Ext.define('CustomApp',
     {
         extend: 'Rally.app.App',
         componentCls: 'app',
-        launch: function() {
-            this._loadData();
+        launch: function () {
+
+            this.filterContainer = Ext.create('Ext.container.Container',
+                {
+                    layout:
+                    {
+                        type: 'hbox',
+                        align: 'stretch'
+                    }
+                });
+
+            this.add(this.filterContainer);
+            this._loadIterationSelection();
+            
         },
     
-        // Get Data From Rally
-        _loadData: function() {
-            var myStore = Ext.create('Rally.data.wsapi.Store',
-                {
-                    model: 'User Story',
-                    autoLoad: true,
-                    listeners:
-                    {
-                        load: function(myStore, data, success) {
-                            console.log('Got Data', myStore, data, success);
-                            this._loadGrid(myStore);
+        // Load iteration combobox selector
+        _loadIterationSelection: function() {
+            this.iterationCombo = Ext.create('Rally.ui.combobox.IterationComboBox',
+                {                    
+                    listeners: {
+                        ready: function (combobox) {
+                            console.log('comboBox', combobox);
+                            console.log('comboBox selected = ', combobox.getRecord().get('_ref'));
+                            this._loadData();
+                        },
+                        select: function (combobox, records) {
+                            console.log('comboBox', combobox);
+                            console.log('comboBox selected = ', combobox.getRecord().get('_ref'));
+                            this._loadData();
                         },
                         scope: this
-                    },
-                    fetch: ['FormattedID', 'Owner', 'Name', 'ScheduleState']
+                    }
                 });
+            this.filterContainer.add(this.iterationCombo);
+        },
+
+        // Get Data From Rally
+        _loadData: function() {
+            var selectedIteration = this.iterationCombo.getRecord().get('_ref');
+
+            var myFilters = [{ property: 'Iteration', operation: '=', value: selectedIteration }];
+
+            //if store exists, just load new data
+            if (this.userStoryStore) {
+                this.userStoryStore.setFilter(myFilters);
+                this.userStoryStore.load();
+            } else {
+                this.userStoryStore = Ext.create('Rally.data.wsapi.Store',
+                    {
+                        model: 'User Story',
+                        autoLoad: true,
+                        filters: myFilters,
+                        listeners:
+                        {
+                            load: function(userStoryStore, myData, success) {
+                                console.log('Got Data', userStoryStore, myData, success);
+                                if (!this.myGrid) {
+                                    this._loadGrid(userStoryStore);
+                                }
+                            },
+                            scope: this
+                        },
+                        fetch: ['FormattedID', 'Owner', 'Name', 'ScheduleState', 'Iteration']
+                    });
+            }
         },
     
         // Create and Show a Grid of given stories
         _loadGrid: function(store) {
-            var myGrid = Ext.create('Rally.ui.grid.Grid',
+            this.myGrid = Ext.create('Rally.ui.grid.Grid',
                 {
                     store: store,
-                    columnCfgs: ['FormattedID', 'Name', 'Owner']
+                    columnCfgs: ['FormattedID', 'Name', 'Owner', 'Iteration']
                 });
 
-            console.log('my grid', myGrid);
-            this.add(myGrid);
+            this.add(this.myGrid);
         }            
         //API Docs: https://help.rallydev.com/apps/2.0/doc/
     });
